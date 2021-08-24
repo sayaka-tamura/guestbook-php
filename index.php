64ae9bb94b1c92f0e7b319f31ffc4529001d5c1c
sayaka-tamura <arccycle@gmail.com>
2021-08-24 10:36:20 +0900

    restore another files

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Guset Book_1.Input</title>
+  </head>
+  <body>
+    <h1>Mail Form_1.Input</h1>
+    <form method="post" action="check.php">
+      <table border="1">
+        <tr>
+          <td>Name</td>
+          <td><input type="text" name="uname" size="30"></td>
+        </tr>
+        <tr>
+          <td>Email Address</td>
+          <td><input type="text" name="email" size="30"></td>
+        </tr>
+        <tr>
+          <td>Message</td>
+          <td>
+            <textarea name="message" cols="30" rows="5"></textarea>
+          </td>
+        </tr>
+        <tr>
+          <td colspan="2">
+            <input type="submit" name="sub1" value="Confirm">
+          </td>
+        </tr>
+      </table>
+    </form>
+  </body>
+</html>
