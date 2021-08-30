41d7aa4f124329883b9b262c8d8ca416dcdadb0c
sayaka-tamura <arccycle@gmail.com>
2021-08-30 22:29:09 +0900

    Connection Test

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
-    phpinfo();
+try{
+	// 接続
+	$db = new PDO('mysql:host=localhost;dbname=guestbook', 'root', 'passward');
+	echo 'データベース接続成功';
+	// 切断
+	$db = null;
+} catch(PDOException $e){
+    echo "データベース接続失敗" . PHP_EOL;
+	echo $e->getMessage();
+	exit;
+}
 
 ?>
