fc8b7f7f3814f06a31d261267d1dd3115740d746
sayaka-tamura <arccycle@gmail.com>
2021-09-01 09:08:47 +0900

    mysqli connection test

--- index.php
+++ index.php
@@ -1,15 +1,28 @@
 <?php
+	// データベースへ接続
+	$db_link = mysqli_connect( 'localhost', 'root', 'password', 'guestbook');
 
-try{
-	// 接続
-	$db = new PDO('mysql:host=localhost;dbname=guestbook', 'root', 'passward');
-	echo 'データベース接続成功';
-	// 切断
-	$db = null;
-} catch(PDOException $e){
-    echo "データベース接続失敗" . PHP_EOL;
-	echo $e->getMessage();
-	exit;
-}
+    if($db_link){
+            echo 'connect success!';
+    } else {
+        echo 'connect fail!';
+    }
+	
+	// 接続エラーの確認
+	if( mysqli_connect_errno($db_link) ) {
+		echo mysqli_connect_errno($db_link) . ' : ' . mysqli_connect_error($db_link);
+	}
 
+	// 接続解除
+	mysqli_close($db_link);
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Example</title>
+    </head>
+    <body>
+        <!-- ここではHTMLを書く以外のことは一切しない -->
+    </body>
+</html>
