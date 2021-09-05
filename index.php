a480830bcf3072e45a1e45423775da20396f5c85
sayaka-tamura <arccycle@gmail.com>
2021-09-05 21:22:55 +0900

    Added options

--- index.php
+++ index.php
@@ -3,11 +3,14 @@
 $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
 $db['dbname'] = ltrim($db['path'], '/');
 $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+$options = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
+);
 
 try {
-    $db = new PDO($dsn, $db['user'], $db['pass']);
-    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db = new PDO($dsn, $db['user'], $db['pass'], $options);
 
     $sql = 'SELECT * FROM user';
     $prepare = $db->prepare($sql);
