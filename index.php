884d0542df3c1af0ac10f77e80fefc006374f3cd
sayaka-tamura <arccycle@gmail.com>
2021-09-05 21:24:07 +0900

    changed DB to message

--- index.php
+++ index.php
@@ -10,9 +10,9 @@ $options = array(
 );
 
 try {
-    $db = new PDO($dsn, $db['user'], $db['pass'], $options);
+    $db = new PDO($dsn, $db['message'], $db['pass'], $options);
 
-    $sql = 'SELECT * FROM user';
+    $sql = 'SELECT * FROM message';
     $prepare = $db->prepare($sql);
     $prepare->execute();
 
