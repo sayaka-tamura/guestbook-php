12e7fee48323991e37066c263d27073fcacbf39f
sayaka-tamura <arccycle@gmail.com>
2021-09-07 23:46:57 +0900

    move php part above footer

--- web/index.php
+++ web/index.php
@@ -1,41 +1,3 @@
-<?php
-
-  $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
-  $db['dbname'] = ltrim($db['path'], '/');
-  $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
-  $options = array(
-      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-      PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
-  );
-
-  try {
-      $db = new PDO($dsn, $db['user'], $db['pass'], $options);
-
-      $sql = 'SELECT * FROM message';
-      $prepare = $db->prepare($sql);
-      $prepare->execute();
-
-      echo '<pre>';
-      $prepare->execute();
-      $result = $prepare->fetchAll(PDO::FETCH_ASSOC);
-      print_r(h($result));
-      echo "\n";
-      echo '</pre>';
-  } catch (PDOException $e) {
-      echo 'Error: ' . h($e->getMessage());
-  }
-
-  function h($var)
-  {
-      if (is_array($var)) {
-          return array_map('h', $var);
-      } else {
-          return htmlspecialchars($var, ENT_QUOTES, 'UTF-8');
-      }
-  }
-?>
-
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <!-- Required meta tags -->
@@ -106,6 +68,42 @@
         </div>
       </form>
 
+      <?php
+        $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+        $db['dbname'] = ltrim($db['path'], '/');
+        $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+        $options = array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
+        );
+
+        try {
+            $db = new PDO($dsn, $db['user'], $db['pass'], $options);
+
+            $sql = 'SELECT * FROM message';
+            $prepare = $db->prepare($sql);
+            $prepare->execute();
+
+            echo '<pre>';
+            $prepare->execute();
+            $result = $prepare->fetchAll(PDO::FETCH_ASSOC);
+            print_r(h($result));
+            echo "\n";
+            echo '</pre>';
+        } catch (PDOException $e) {
+            echo 'Error: ' . h($e->getMessage());
+        }
+
+        function h($var)
+        {
+            if (is_array($var)) {
+                return array_map('h', $var);
+            } else {
+                return htmlspecialchars($var, ENT_QUOTES, 'UTF-8');
+            }
+        }
+      ?>
 
     </main>
     <footer class="my-5 text-white-50">
