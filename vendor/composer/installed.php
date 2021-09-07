729174b2289f7e52cf70f7fa343e29e0808cf75c
sayaka-tamura <arccycle@gmail.com>
2021-09-07 22:07:21 +0900

    add composer & Procfile

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '7bcca8f1af96fe59ad57bf8ad36a9c2d6e8c06d9',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '7bcca8f1af96fe59ad57bf8ad36a9c2d6e8c06d9',
+            'dev_requirement' => false,
+        ),
+    ),
+);
