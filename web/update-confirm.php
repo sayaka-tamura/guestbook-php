713eeb756798ec76dcf06e5306c2b39b81150a1d
sayaka-tamura <arccycle@gmail.com>
2021-09-11 15:57:45 +0900

    Added Update function

--- web/update-confirm.php
+++ web/update-confirm.php
@@ -1,50 +1,33 @@
 <?php
-// Session Start
-session_start();
-if (empty($_SESSION)) {
-  echo "Ended this process";
-  exit;
-}
-// 接続設定
-$dbtype = "mysql";
-$sv = "localhost";
-$dbname = "guestbook";
-$user = "root";
-$pass = "password";
+  // Session Start
+  session_start();
 
-// DB に接続
-$dsn = "$dbtype:dbname=$dbname;host=$sv";
-$conn = new PDO($dsn, $user, $pass);
+  // 変更内容の取得・検証・加工
+  $m_name = chkString($_POST["m_name"], "Name");
+  $m_mail = chkString($_POST["m_mail"], "E-mail address", true); // ture -> check 省略
+  $m_message = chkString($_POST["m_message"], "Message");
 
-// 変更内容の取得（変更データの主キーも含む）
-$m_id = $_SESSION["m_id"];
-$m_name = htmlspecialchars($_SESSION["m_name"], ENT_QUOTES, "UTF-8");
-$m_mail = htmlspecialchars($_SESSION["m_mail"], ENT_QUOTES, "UTF-8");
-$m_message = htmlspecialchars($_SESSION["m_message"], ENT_QUOTES, "UTF-8");
+  // 変更内容をセッション変数に格納
+  $_SESSION["m_name"] = $m_name;
+  $_SESSION["m_mail"] = $m_mail;
+  $_SESSION["m_message"] = $m_message;
 
-// データの追加
-$sql = "UPDATE message SET m_name=:m_name, m_mail=:m_mail, m_message=:m_message, m_dt=NOW() WHERE m_id=:m_id";
-$stmt = $conn->prepare($sql);
-$stmt->bindParam(":m_id", $m_id);
-$stmt->bindParam(":m_name", $m_name);
-$stmt->bindParam(":m_mail", $m_mail);
-$stmt->bindParam(":m_message", $m_message);
-$stmt->execute();
+  // 入力値の検証・加工
+  function chkString($temp = "", $field, $accept_empty = false)
+  {
+    // 未入力チェック
+    if (empty($temp) and !$accept_empty) {
+      echo $field . " には何か入力してください。";
+      exit;
+    }
 
-// エラーチェック
-$error = $stmt->errorInfo();
-if ($error[0] != "00000") {
-  $message = "データの追加に失敗しました。{$error[2]}";
-} else {
-  $message = "データを変更しました。";
-}
+    // 入力内容を安全な値に
+    $temp = htmlspecialchars($temp, ENT_QUOTES, "UTF-8");
 
-// セッションデータの破棄
-$_SESSION = array();
-session_destroy();
+    // 加工後の文字列を返す
+    return $temp;
+  }
 ?>
-
-<!-- 処理結果の表示 -->
 <html>
 
 <head>
@@ -72,6 +55,7 @@ session_destroy();
   </style>
   <!-- Custom styles for this template -->
   <link href="css/cover.css" rel="stylesheet">
+
 </head>
 
 <body class="d-flex h-100 text-center text-white bg-dark">
@@ -89,10 +73,10 @@ session_destroy();
     </header>
 
     <main class="px-3 my-5">
-      <p>Update Completion Page</p>
+      <p>Confirmation for Update Page</p>
+      <form method="post" action="update-submit.php">
         <div class="form-group">
-          <p><?php echo $message; ?></p>
-          <table class=" mx-auto mx-auto my-3 col-6">
+          <table class=" mx-auto mx-auto my-4 col-6">
             <tr class="my-2">
               <td class="my-1">Name</td>
               <td><?php echo $m_name; ?></td>
@@ -101,21 +85,22 @@ session_destroy();
               <td class="my-1">Email Address</td>
               <td><?php echo $m_mail; ?></td>
             </tr>
-            <tr class="my-2">
+            <tr>
               <td class="my-1">Message</td>
               <td><?php echo nl2br($m_message); ?></td>
             </tr>
-            <td colspan="2">
-              <input type="button" class="form-control mt-3 btn btn-info" value="To Top Page" onClick="location.href='index.php'">
-            </td>
+            <tr class="my-2">
+              <td colspan="2">
+                <input type="submit" class="form-control mt-3 btn btn-info" value="Update">
+              </td>
+            </tr>
           </table>
-        </div>
+      </form>
     </main>
     <footer class="my-5 text-white-50">
       <p>Cover template for <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
     </footer>
   </div>
-  
   <!-- Option 1: Bootstrap Bundle with Popper -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </body>
--- web/update-submit.php
+++ web/update-submit.php
@@ -1,33 +1,45 @@
 <?php
-// Session Start
-session_start();
-
-// 変更内容の取得・検証・加工
-$m_name = chkString($_POST["m_name"], "Name");
-$m_mail = chkString($_POST["m_mail"], "E-mail address", true); // ture -> check 省略
-$m_message = chkString($_POST["m_message"], "Message");
-
-// 変更内容をセッション変数に格納
-$_SESSION["m_name"] = $m_name;
-$_SESSION["m_mail"] = $m_mail;
-$_SESSION["m_message"] = $m_message;
-
-// 入力値の検証・加工
-function chkString($temp = "", $field, $accept_empty = false)
-{
-  // 未入力チェック
-  if (empty($temp) and !$accept_empty) {
-    echo $field . " には何か入力してください。";
+  // Session Start
+  session_start();
+  if (empty($_SESSION)) {
+    echo "Ended this process";
     exit;
   }
 
-  // 入力内容を安全な値に
-  $temp = htmlspecialchars($temp, ENT_QUOTES, "UTF-8");
+  require("dbconnect.php");
 
-  // 加工後の文字列を返す
-  return $temp;
-}
+  //DB接続関数を dbconnet.php から呼び出して接続
+  $db = dbConnect();
+
+  // 変更内容の取得（変更データの主キーも含む）
+  $m_id = $_SESSION["m_id"];
+  $m_name = htmlspecialchars($_SESSION["m_name"], ENT_QUOTES, "UTF-8");
+  $m_mail = htmlspecialchars($_SESSION["m_mail"], ENT_QUOTES, "UTF-8");
+  $m_message = htmlspecialchars($_SESSION["m_message"], ENT_QUOTES, "UTF-8");
+
+  // データの追加
+  $sql = "UPDATE message SET m_name=:m_name, m_mail=:m_mail, m_message=:m_message, m_dt=NOW() WHERE m_id=:m_id";
+  $stmt = $db->prepare($sql);
+  $stmt->bindParam(":m_id", $m_id);
+  $stmt->bindParam(":m_name", $m_name);
+  $stmt->bindParam(":m_mail", $m_mail);
+  $stmt->bindParam(":m_message", $m_message);
+  $stmt->execute();
+
+  // エラーチェック
+  $error = $stmt->errorInfo();
+  if ($error[0] != "00000") {
+    $message = "データの追加に失敗しました。{$error[2]}";
+  } else {
+    $message = "データを変更しました。";
+  }
+
+  // セッションデータの破棄
+  $_SESSION = array();
+  session_destroy();
 ?>
+
+<!-- 処理結果の表示 -->
 <html>
 
 <head>
@@ -55,7 +67,6 @@ function chkString($temp = "", $field, $accept_empty = false)
   </style>
   <!-- Custom styles for this template -->
   <link href="css/cover.css" rel="stylesheet">
-
 </head>
 
 <body class="d-flex h-100 text-center text-white bg-dark">
@@ -73,10 +84,10 @@ function chkString($temp = "", $field, $accept_empty = false)
     </header>
 
     <main class="px-3 my-5">
-      <p>Confirmation for Update Page</p>
-      <form method="post" action="update-submit.php">
+      <p>Update Completion Page</p>
         <div class="form-group">
-          <table class=" mx-auto mx-auto my-4 col-6">
+          <p><?php echo $message; ?></p>
+          <table class=" mx-auto mx-auto my-3 col-6">
             <tr class="my-2">
               <td class="my-1">Name</td>
               <td><?php echo $m_name; ?></td>
@@ -85,22 +96,21 @@ function chkString($temp = "", $field, $accept_empty = false)
               <td class="my-1">Email Address</td>
               <td><?php echo $m_mail; ?></td>
             </tr>
-            <tr>
+            <tr class="my-2">
               <td class="my-1">Message</td>
               <td><?php echo nl2br($m_message); ?></td>
             </tr>
-            <tr class="my-2">
-              <td colspan="2">
-                <input type="submit" class="form-control mt-3 btn btn-info" value="Update">
-              </td>
-            </tr>
+            <td colspan="2">
+              <input type="button" class="form-control mt-3 btn btn-info" value="To Top Page" onClick="location.href='index.php'">
+            </td>
           </table>
-      </form>
+        </div>
     </main>
     <footer class="my-5 text-white-50">
       <p>Cover template for <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
     </footer>
   </div>
+  
   <!-- Option 1: Bootstrap Bundle with Popper -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </body>
