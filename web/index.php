93a7028190cfeee3356f56510d438c227900809f
sayaka-tamura <arccycle@gmail.com>
2021-09-11 23:47:38 +0900

    added head.php with php

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <html>
   
-  require("head.php");
+  <?php require("head.php"); ?>
   
   <body class="d-flex h-100 text-center text-white bg-dark">
     <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
